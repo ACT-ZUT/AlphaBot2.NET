07eb6b9e773565416359519c6c3ff6018566e8b5
Dawid <dawd785@gmail.com>
2020-03-30 09:44:23 +0200

    Implementation of buffered channel reading

--- Tlc1543/Tlc1543.cs
+++ Tlc1543/Tlc1543.cs
@@ -23,60 +23,151 @@ namespace Iot.Device.Adc
 	/// </summary>
 	public class Tlc1543 : IDisposable
 	{
-		public GpioController digital = new GpioController(PinNumberingScheme.Logical);
-		public byte CS, DOUT, ADDR, IOCLK, numSensors;
-		public int calibratedMin, calibratedMax;
-		public SortedList<byte, uint> last_values = new SortedList<byte, uint>();
-		public SortedList<byte, uint> values = new SortedList<byte, uint>();
+		private GpioController digital = new GpioController(PinNumberingScheme.Logical);
+		
+		public SortedList<byte, uint> values = new SortedList<byte, uint>(11);
+		public SortedList<byte, uint> last_values = new SortedList<byte, uint>(11);
+		private byte CS, DOUT, ADDR, IOCLK, EOC, channels = 11;
+		public Channel chargeChannel = Channel.SelfTest512;
 
 		/// <summary>
-		/// the number of single ended input channel on the ADC
+		/// 
 		/// </summary>
-		public Tlc1543 (byte CS, byte DOUT, byte ADDR, byte IOCLK, byte numSensors)
+		/// <param name="ADDR">Address</param>
+		/// <param name="CS">Chip Select</param>
+		/// <param name="DOUT">Data Out</param>
+		/// <param name="IOCLK">I/O Clock</param>
+		public Tlc1543(byte ADDR, byte CS, byte DOUT, byte IOCLK)
 		{
-			this.numSensors = numSensors;
-			for (byte i = 0; i < numSensors; i++)
-			{
-				values.Add(i, 0);
-				last_values.Add(i, 0);
-			}
-			this.calibratedMin = 0 * numSensors;
-			this.calibratedMax = 1023 * numSensors;
-			
+			this.ADDR = ADDR;
 			this.CS = CS;
 			this.DOUT = DOUT;
-			this.ADDR = ADDR;
 			this.IOCLK = IOCLK;
+
+			digital.OpenPin(ADDR, PinMode.Output);
 			digital.OpenPin(CS, PinMode.Output);
 			digital.OpenPin(DOUT, PinMode.InputPullUp);
+			digital.OpenPin(IOCLK, PinMode.Output);
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="ADDR">Address</param>
+		/// <param name="CS">Chip Select</param>
+		/// <param name="DOUT">Data Out</param>
+		/// <param name="EOC">End of Conversion</param>
+		/// <param name="IOCLK">I/O Clock</param>
+		public Tlc1543(byte ADDR, byte CS, byte DOUT, byte IOCLK, byte EOC)
+		{
+			this.ADDR = ADDR;
+			this.CS = CS;
+			this.DOUT = DOUT;
+			this.EOC = EOC;
+			this.IOCLK = IOCLK;
+			
 			digital.OpenPin(ADDR, PinMode.Output);
+			digital.OpenPin(CS, PinMode.Output);
+			digital.OpenPin(DOUT, PinMode.InputPullUp);
+			digital.OpenPin(EOC, PinMode.InputPullUp);
 			digital.OpenPin(IOCLK, PinMode.Output);
 		}
 
+
 		/// <summary>
-		/// 	Reads the sensor values into an array. There *MUST* be space
-		/// for as many values as there were sensors specified in the constructor.
-		/// Example usage:
-		/// unsigned int sensor_values[8];
-		/// sensors.read(sensor_values);
+		/// Reads the sensor values into an array. 
 		/// The values returned are a measure of the reflectance in abstract units,
 		/// with higher values corresponding to lower reflectance(e.g.a black
 		/// surface or a void).
 		/// </summary>
+		/// <returns>A value corresponding to relative voltage level on specified device channel</returns>
 		public SortedList<byte, uint> AnalogRead()
 		{
-			//SortedList<byte, uint> values = new SortedList<byte, uint>(numSensors);
-			for (byte i = 0; i < numSensors; i++)
+			for (byte i = 0; i < channels + 1; i++)
 			{
 				values[i] = 0;
 			}
-			for (byte j = 0; j < numSensors; j++)
+			for (byte channel = 0; channel < channels; channel++)
 			{
 				digital.Write(CS, 0);
-				for (int i = 0; i < 4; i++)
+				for (int i = 0; i < 10; i++)
+				{
+					if (i < 4)
+					{
+						//send 4-bit Address
+						if ((channel >> (3 - i) & 0x01) != 0)
+						{
+							digital.Write(ADDR, 1);
+						}
+						else
+						{
+							digital.Write(ADDR, 0);
+						}
+					}
+					// read 10-bit data
+					values[channel] <<= 1;
+					if (digital.Read(DOUT) == PinValue.High)
+					{
+						values[channel] |= 0x01;
+					}
+					digital.Write(IOCLK, 1);
+					digital.Write(IOCLK, 0);
+				}
+				Thread.Sleep(TimeSpan.FromMilliseconds(0.1));
+				digital.Write(CS, 1);
+			}
+			last_values = values;
+			return values;
+		}
+
+		/// <summary>
+		/// Reads the sensor values into an integer.
+		/// The values returned are a measure of the reflectance in abstract units,
+		/// with higher values corresponding to lower reflectance(e.g.a black
+		/// surface or a void).
+		/// </summary>
+		/// <param name="channelNumber"></param>
+		/// <returns>A 10 bit value corresponding to relative voltage level on specified device channel</returns>
+		public int ReadChannel(Channel channelNumber)
+		{
+			Read(channelNumber);
+			return Read(chargeChannel);
+		}
+
+		/// <summary>
+		/// Reads the sensor values into an List
+		/// The values returned are a measure of the reflectance in abstract units,
+		/// with higher values corresponding to lower reflectance(e.g.a black
+		/// surface or a void).
+		/// </summary>
+		/// <param name="channelList">List of channels to read</param>
+		/// <returns>List of 10 bit values corresponding to relative voltage level on specified device channels</returns>
+		public List<int> ReadChannel(List<Channel> channelList)
+		{
+			List<int> values = new List<int>(channelList.Count);
+			Read(channelList[0]);
+			for (int i = 1; i < channelList.Count; i++)
+			{
+				values.Add(Read(channelList[i]));
+			}
+			values.Add(Read(chargeChannel));
+			return values;
+		}
+
+		private int Read(Channel channelNumber)
+		{
+			int value = 0;
+			digital.Write(CS, 0);
+			//1.425uS between CS => 0 and first ADDR
+			//probably can omit that due to for loop and if condition
+			//need to check CS(less than 0.8V) to first ADDR(minimum of 2V)
+			//on osciloscope to check how much time it takes on RPi
+			DelayHelper.DelayMicroseconds(1, false); 
+			for (int i = 0; i < 10; i++)
+			{
+				if (i < 4)
 				{
 					//send 4-bit Address
-					if ((j >> (3 - i) & 0x01) != 0)
+					if (((byte)channelNumber >> (3 - i) & 0x01) != 0)
 					{
 						digital.Write(ADDR, 1);
 					}
@@ -84,31 +175,21 @@ namespace Iot.Device.Adc
 					{
 						digital.Write(ADDR, 0);
 					}
-					// read MSB 4-bit data
-					values[j] <<= 1;
-					if(digital.Read(DOUT) == PinValue.High)
-					{
-						values[j] |= 0x01;
-					}
-					digital.Write(IOCLK, 1);
-					digital.Write(IOCLK, 0);
 				}
-				for (int i = 0; i < 6; i++)
+				// read 10-bit data (from earlier loop)
+				value <<= 1;
+				if (digital.Read(DOUT) == PinValue.High)
 				{
-					// read LSB 8-bit data
-					values[j] <<= 1;
-					if(digital.Read(DOUT) == PinValue.High)
-					{
-						values[j] |= 0x01;
-					}
-					digital.Write(IOCLK, 1);
-					digital.Write(IOCLK, 0);
+					value |= 0x01;
 				}
-				Thread.Sleep(TimeSpan.FromMilliseconds(0.1));
+				//time from ADDR to IOCLK minimum 100ns
 				digital.Write(IOCLK, 1);
+				digital.Write(IOCLK, 0);
+				//to check how fast can those clocks can be generated
 			}
-			last_values = values;
-			return values;
+			digital.Write(CS, 1);
+			DelayHelper.DelayMicroseconds(100, false);
+			return value;
 		}
 
 		/// <summary>
@@ -149,8 +230,84 @@ namespace Iot.Device.Adc
 		/// </summary>
 		public void Dispose()
 		{
-			digital?.Dispose();
+			//digital?.Dispose();
 			digital = null;
+		}
+
+		public enum Channel
+		{
+			/// <summary>
+			/// Channel A0
+			/// </summary>
+			A0 = 0,
+
+			/// <summary>
+			/// 
+			/// </summary>
+			A1 = 1,
+
+			/// <summary>
+			/// 
+			/// </summary>
+			A2 = 2,
+
+			/// <summary>
+			/// 
+			/// </summary>
+			A3 = 3,
+
+			/// <summary>
+			/// 
+			/// </summary>
+			A4 = 4,
+
+			/// <summary>
+			/// 
+			/// </summary>
+			A5 = 5,
+
+			/// <summary>
+			/// 
+			/// </summary>
+			A6 = 6,
+
+			/// <summary>
+			/// 
+			/// </summary>
+			A7 = 7,
+
+			/// <summary>
+			/// 
+			/// </summary>
+			A8 = 8,
+
+			/// <summary>
+			/// 
+			/// </summary>
+			A9 = 9,
+
+			/// <summary>
+			/// 
+			/// </summary>
+			A10 = 10,
+
+			/// <summary>
+			/// Self Test channel that should charge capacitors to (Vref+ - Vref-)/2
+			/// Gives out value of (dec)512
+			/// </summary>
+			SelfTest512 = 11,
+
+			/// <summary>
+			/// Self Test channel that should charge capacitors to Vref-
+			/// /// Gives out value of (dec)0
+			/// </summary>
+			SelfTest0 = 12,
+
+			/// <summary>
+			/// Self Test channel that should charge capacitors to Vref+
+			/// /// Gives out value of (dec)1023
+			/// </summary>
+			SelfTest1024 = 13,
 		}
 	}
 }
