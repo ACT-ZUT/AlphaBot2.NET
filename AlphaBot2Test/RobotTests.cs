c7a0f8bcd78591cc02f86d4421a768826d2aa5cc
SaDiablo <dawd785@gmail.com>
2020-09-14 01:57:47 +0200

    Stashing fix

--- AlphaBot2Test/RobotTests.cs
+++ AlphaBot2Test/RobotTests.cs
@@ -1,31 +1,28 @@
 using NUnit.Framework;
 using System;
 using System.IO;
-using AlphaBot2;
 using System.Collections.Generic;
 using System.Linq;
+
+using AlphaBot2;
 
 namespace AlphaBot2Test
 {
     public class RobotTests
     {
-        private AlphaBot2.AlphaBot2 _robot;
+        private AlphaBot _robot;
         [SetUp]
         public void Setup()
         {
-<<<<<<< Updated upstream
-            _robot = new AlphaBot2.AlphaBot2(new List<string>() { "adc", "10" });
-
-=======
             _robot = new AlphaBot();
             _robot.SetParameter(AlphaBot.Parameters.MainSpeed, 10);
->>>>>>> Stashed changes
         }
 
         [TestCase(new int[] { 50, 1000, 1000, 1000, 1000 }, ExpectedResult = -100)]
         [TestCase(new int[] { 50, 50, 1000, 1000, 1000 }, ExpectedResult = -75)]
         [TestCase(new int[] { 50, 50, 50, 1000, 1000 }, ExpectedResult = -50)]
         [TestCase(new int[] { 1000, 50, 50, 1000, 1000 }, ExpectedResult = -25)]
+        [TestCase(new int[] { 1000, 1000, 50, 1000, 1000 }, ExpectedResult = 0)]
         [TestCase(new int[] { 1000, 50, 50, 50, 1000 }, ExpectedResult = 0)]
         [TestCase(new int[] { 50, 50, 50, 50, 50 }, ExpectedResult = 0)]
         [TestCase(new int[] { 1000, 1000, 50, 50, 1000 }, ExpectedResult = 25)]
@@ -35,13 +32,11 @@ namespace AlphaBot2Test
         public double TestADCtoLine(int[] valuesArray)
         {
             List<int> values = valuesArray.ToList();
-            var ADCLineValue = _robot.FindLine(values);
+            var ADCLineValue = _robot.GetLineValue(values);
             var result = (ADCLineValue >= -100) & (ADCLineValue <= 100);
             Assert.IsTrue(result, "ADC Line Value out of Range");
             return (double)ADCLineValue;
         }
-<<<<<<< Updated upstream
-=======
 
         [TestCase(100.0, ExpectedResult = 100)]
         [TestCase(30.0, ExpectedResult = 40)]
@@ -86,6 +81,5 @@ namespace AlphaBot2Test
             Assert.IsTrue(result, "ADC Line Value out of Range");
             return (double)speedL;
         }
->>>>>>> Stashed changes
     }
 }
